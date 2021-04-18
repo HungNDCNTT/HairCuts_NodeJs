68cd66107f391c18ff7006f22fbc19d120969336
Nguyen Van Tan <tan.nguyenvan@tapbi.vn>
2021-04-18 21:56:45 +0700

    dsdaa

--- routers/common-router.js
+++ routers/common-router.js
@@ -4,101 +4,113 @@ const storage = require("node-sessionstorage");
 let router = express.Router();
 
 router.get('/', (req, res) => {
-  if(storage.getItem("email") != null) {
-    res.render('home');
-  } else {
-    res.render('login');
-  }
+    if (storage.getItem("email") != null) {
+        res.render('home');
+    } else {
+        res.render('login');
+    }
 })
 router.get('/login', (req, res) => {
-  if (storage.getItem("email") != null) {
-    res.render('home');
-  } else {
-    res.render('login');
-  }
+    if (storage.getItem("email") != null) {
+        res.render('home');
+    } else {
+        res.render('login');
+    }
 })
 
 router.get('/register', (req, res) => {
-  if (storage.getItem("email") != null) {
-    res.render('home');
-  } else {
-    res.render('register');
-  }
+    if (storage.getItem("email") != null) {
+        res.render('home');
+    } else {
+        res.render('register');
+    }
 })
 
 
 router.get('/logout', (req, res) => {
-  storage.setItem("email", null);
-  storage.setItem("isLogin", false);
-  res.render("login");
+    storage.setItem("email", null);
+    storage.setItem("isLogin", false);
+    res.render("login");
 })
 //router.get('/order', (req, res) => res.render('order'));
 // router.get('/user', (req, res) => res.render('user'));
 router.get('/recover', (req, res) => res.render('recover'));
 var abc;
 var valueQuery;
-router.get('/home', (req  , res) => {
-  // if (storage.getItem('email') != null) {
-   abc = req.query.gender
+router.get('/home', (req, res) => {
+    // if (storage.getItem('email') != null) {
+    abc = req.query.gender
 
-  if (abc =="wait"){
-    valueQuery = "wait"
-  }else if (abc =="done"){
-    valueQuery = "done"
-  }else {
-    abc = "pedding"
-    valueQuery = "booking"
-  }
-  BookingList.find({status: valueQuery}, function (err, bookings) {
-    res.render("home", {dataa: bookings, select : abc});
-    // res.render('booking', {
-    //   title: 'Manager Booking',
-    //   bookings: bookings
-    // });
-  });
+    if (abc == "wait") {
+        valueQuery = "wait"
+    } else if (abc == "done") {
+        valueQuery = "done"
+    } else {
+        abc = "pedding"
+        valueQuery = "booking"
+    }
+    BookingList.find({status: valueQuery}, function (err, bookings) {
+        res.render("home", {dataa: bookings, select: abc});
+        // res.render('booking', {
+        //   title: 'Manager Booking',
+        //   bookings: bookings
+        // });
+    });
 
-  // } else {
-  //   res.render("login");
-  // }
+    // } else {
+    //   res.render("login");
+    // }
 });
 
 let BookingList = require('../models/booking');
-
+let HairDresser = require('../models/hair_dresser');
 
 router.post('/home', (req, res) => {
     var varrrrr = ""
     var aa = ""
-  switch (req.body.change){
-    case "Confirm":
-        varrrrr = "wait"
-      break;
-    case "Deny":
-        aa = "deny"
-        varrrrr = "done"
-      break;
-    case "Completed":
-        varrrrr = "done"
-      break;
-  }
+    switch (req.body.change) {
+        case "Confirm":
+            varrrrr = "wait"
+            break;
+        case "Deny":
+            aa = "deny"
+            varrrrr = "done"
+            break;
+        case "Completed":
+            varrrrr = "done"
+            break;
 
-    BookingList.findByIdAndUpdate(
-        req.body.abccc,
-        {
-            status: varrrrr,
-            result: aa,
-        },function(err, response){
-            BookingList.find({status: valueQuery}, function (err, bookings) {
-                res.render("home", {dataa: bookings, select : abc});
+        case "View":
+            varrrrr = "View"
+            break;
+    }
+
+    if (varrrrr == ("View")) {
+        BookingList.findOne({_id: req.body.abccc}, function (err, detailById) {
+            HairDresser.findOne({_id: detailById.dresser_id}, function (err, dresserById) {
+                res.render("user",{dataBook: detailById, dataDreser: dresserById});
             });
-        }
-    );
+        });
+    } else {
+        BookingList.findByIdAndUpdate(
+            req.body.abccc,
+            {
+                status: varrrrr,
+                result: aa,
+            }, function (err, response) {
+                BookingList.find({status: valueQuery}, function (err, bookings) {
+                    res.render("home", {dataa: bookings, select: abc});
+                });
+            }
+        );
+    }
 
 
-  // if (storage.getItem('email') != null) {
-  //   res.render("home");
-  // } else {
-  //   res.render("login");
-  // }
+    // if (storage.getItem('email') != null) {
+    //   res.render("home");
+    // } else {
+    //   res.render("login");
+    // }
 });
 router.get('/page-lockscreen', (req, res) => res.render('page/page-lockscreen'));
 router.get('/profile', (req, res) => res.render('profile'));
